170950774842a882773895b7ef155468368f1b87
Gian05JV <126518912+Gian05JV@users.noreply.github.com>
2023-04-10 21:47:16 -0600

    Update main2.py

--- main2.py
+++ main2.py
@@ -4,7 +4,7 @@ import monitoreo
 
 app = Flask(__name__)
 monitoreo.create_dash(app)
-client = MongoClient('mongodb://192.168.0.13:27017/')
+client = MongoClient('mongodb://192.168.0.13:27017/?serverSelectionTimeoutMS=5000')
 db = client.dbisesion
 users = db.users
 
